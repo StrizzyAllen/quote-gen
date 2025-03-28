d21e21c7cd2cc7d57ab87d6770cadc826b375620
Isaiah Strouse <isaiahstrouse66@gmail.com>
2025-03-27 23:11:30 -0500

    switched to json file for quotes

--- script.js
+++ script.js
@@ -4,27 +4,19 @@ const quoteText = document.querySelector('.quote');
 const authorText = document.querySelector('.author');
 
 async function getQuote() {
-    const url = "https://api.api-ninjas.com/v1/quotes";
-    const apiKey = '4biy4XmWTDFuLNYLIy3HCQ==oJtWqEVNKDr3uYPq';
+    const url = "quotes.json";
 
-    try {
-        quoteText.textContent = "Loading....";
-        authorText.textContent = "";
-        const response = await fetch(url, {
-            headers: {
-                'X-Api-Key': apiKey
-            }
-        });
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
+    quoteText.textContent = "Loading....";
+    authorText.textContent = "";
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const json = await response.json();
-        quoteText.textContent = json[0].quote;
-        authorText.textContent = `— ${json[0].author}`;
-    } catch (error) {
-        console.error(error.message);
-    }
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const quoteData = data[randomIndex];
+
+    quoteText.textContent = quoteData.quote;
+    authorText.textContent = quoteData.author;
+
 }
 
 submitButton.addEventListener('click', () => {
